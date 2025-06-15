cfb47d25584d3279450162b17d2418edafb8209e
aoge2716 <aoge2716@gmail.com>
2025-06-15 23:59:03 +0200

    Connect MongoDB and improve setup with async/await

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,31 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
-
+  
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+
+const startServer = async () => {
+  try{
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("mongoDB connected");
+
+    app.listen(port, ()=>{
+      console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+    });
+  } catch (error){
+    console.error("mongoDB Connection Error: ", error);
+    process.exit(1);
+  };
+};
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+startServer();
