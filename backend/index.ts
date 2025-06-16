4c5ad3901263a201807e0fe3a609a1c57ca5ffc2
aoge2716 <aoge2716@gmail.com>
2025-06-16 02:20:47 +0200

    Enable CORS for frontend-backend communication

--- backend/index.ts
+++ backend/index.ts
@@ -2,14 +2,15 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cityRoutes from './src/routes/cityRoutes';
+import cors from 'cors';
 
 dotenv.config();
   
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());  
 app.use(express.json());
-
 app.use('/cities', cityRoutes);
 
 const startServer = async () => {
