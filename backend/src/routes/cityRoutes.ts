f425ff988f0231b58a2a0dd854869c5d852408be
aoge2716 <aoge2716@gmail.com>
2025-06-16 03:10:24 +0200

    imiplement full backend CRUD for cities

--- backend/src/routes/cityRoutes.ts
+++ backend/src/routes/cityRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import City from '../models/City';
+import { Request, Response } from 'express';
 
 const router = express.Router();
 
@@ -27,6 +28,42 @@ router.get('/', async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch cities' });
+  }
+});
+
+// POST /cities
+router.post('/', async (req, res) => {
+  try{
+    const{ uuid, cityName, count } = req.body;
+    const newCity = await City.create({ uuid, cityName, count});
+    res.status(201).json(newCity);
+  } catch (err){
+    res.status(500).json({error: 'failed to add city'})
+  }
+});
+
+// PUT /cities:id
+router.put('/:id', async (req: Request<{ id: string }>, res: Response) => {
+  try{
+    const updatedCity = await City.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedCity) return res.status(404).json({ error: 'City not found' });
+    res.json(updatedCity);
+  } catch (err) {
+      res.status(500).json({ error: 'Failed to update city' });
+  }
+});
+
+// DELETE /cities/:id
+router.delete('/:id', async (req: Request<{ id: string }>, res: Response) => {
+  try {
+    const deleted = await City.findByIdAndDelete(req.params.id);
+    if (!deleted) return res.status(404).json({ error: 'City not found' });
+    res.json({ message: 'City deleted' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete city' });
   }
 });
 
